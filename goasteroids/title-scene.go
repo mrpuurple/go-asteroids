81e6fea250756d920ffa8ba6f8584ec25a85aedd
Nichlas Handstedt <nichlas.handstedt@scania.com>
2025-03-02 01:03:11 +0100

    fix depricated text packages

--- goasteroids/title-scene.go
+++ goasteroids/title-scene.go
@@ -4,18 +4,28 @@ import (
 	"asteroids/assets"
 	"image/color"
 
-	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
 	"golang.org/x/image/font"
 )
 
-type TitleScene struct {}
+type TitleScene struct{}
 
 func (t *TitleScene) Draw(screen *ebiten.Image) {
 	textToDraw := "1 coin = 1 play"
-	tw := widthOfText(assets.TitleFont, textToDraw)
-	text.Draw(screen, textToDraw, assets.TitleFont, ScreenWidth/2-tw/2, ScreenHeight-200, color.White)
+
+	op := &text.DrawOptions{
+		LayoutOptions: text.LayoutOptions{
+			PrimaryAlign: text.AlignCenter,
+		},
+	}
+	op.ColorScale.ScaleWithColor(color.White)
+	op.GeoM.Translate(float64(ScreenWidth/2), ScreenHeight-200)
+	text.Draw(screen, textToDraw, &text.GoTextFace{
+		Source: assets.TitleFont,
+		Size:   48,
+	}, op)
 }
 
 func (t *TitleScene) Update(state *State) error {
