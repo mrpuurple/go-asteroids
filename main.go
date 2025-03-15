bcde90444422dce15eccdf0b6b3103e04368d83b
Nichlas Handstedt <nichlas.handstedt@scania.com>
2025-03-15 19:32:05 +0100

    build

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ func main() {
 	ebiten.SetWindowTitle("Asteroids")
 	ebiten.SetWindowSize(goasteroids.ScreenWidth, goasteroids.ScreenHeight)
 
+	// Hide the cursor.
+	ebiten.SetCursorMode(ebiten.CursorModeHidden)
+
+	// Set to full screen.
+	ebiten.SetFullscreen(true)
+
 	err := ebiten.RunGame(&goasteroids.Game{})
 	if err != nil {
 		panic(err)
